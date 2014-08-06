1f7ec852c26d87067524f8a2062efb7ac1dc07cd
ukyoi <ukyoid@gmail.com>
2014-08-06 17:07:13 +0800

    Add two function used for printing info.

--- test.js
+++ test.js
@@ -24,11 +24,41 @@ function showPlate(plate) {
 	console.log(toOutput);
 }
 
+function showBactInfo(bact, onlyShowNum) {
+	if (bact == undefined) {
+		console.log("Bact is undefined.");
+		return;
+	}
+	var toOut = '';
+	toOut += 'teamNum:\t' + bact.teamNum + '\n';
+	if (onlyShowNum == true) {
+		return;
+	} // else: show other info.
+	toOut += 'prolLv:\t' + bact.proliferativeLv + '\n'
+	toOut += 'aggrLv:\t' + bact.aggressiveLv + '\n';
+	toOut += 'variLv:\t' + bact.variativeLv + '\n';
+	toOut += 'longLv:\t' + bact.longevityLv + '\n';
+	console.log(toOut);
+}
+
+function showLatticeInfo(plate, row, col)
+{
+	var lattice = plate.latticeArray[row][col];
+	console.log('Bact in the lattice ' + '[' + row + ', ' + col + '] :\t');
+	(lattice.bact==undefined) ? console.log('No bact here.') : showBactInfo(lattice.bact);
+	/*
+	 * TODO: show competitors.
+	console.log('Competitors:\t');
+	console.log(lattice.competitor);
+	*/
+}
+
 thePlate = new ThePlate();
-showPlate(thePlate);
 
 // For Testing:
 /*
+showPlate(thePlate);
+
 thePlate.addNewPlayer(10,20,30,40, 2, 1);
 thePlate.addNewPlayer(20,30,40,10, 10, 5);
 thePlate.addNewPlayer(30,40,10,20, 15, 15);
@@ -38,5 +68,7 @@ thePlate.aging();
 showPlate(thePlate);
 console.log(thePlate.teamList);
 console.log(thePlate.teamList);
-*/
 
+showBactInfo(thePlate.latticeArray[2][1].bact);
+showLatticeInfo(thePlate, 2, 1);
+*/
